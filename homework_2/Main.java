5596db3961bfe0130c0660477c6d9a26dc7fe32d
Pashkov Pavel <109415423+PavelPaSer@users.noreply.github.com>
2023-06-29 14:09:13 +0300

    Add files via upload

---
+++ homework_2/Main.java
@@ -0,0 +1,35 @@
+// Напишите программу, которая запрашивает у пользователя число и проверяет, является ли оно положительным. 
+// Если число отрицательное или равно нулю, программа должна выбрасывать 
+// исключение InvalidNumberException с сообщением "Некорректное число". 
+// В противном случае, программа должна выводить сообщение "Число корректно".
+
+
+package homework_2;
+import java.util.Scanner;
+
+class InvalidNumberException extends Exception {
+    public InvalidNumberException(String message) {
+        super(message);
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        try {
+            checkPositiveNumber();
+        } catch (InvalidNumberException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public static void checkPositiveNumber() throws InvalidNumberException {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Введите число: ");
+        double number = scanner.nextDouble();
+        if (number <= 0) {
+            throw new InvalidNumberException("Некорректное число");
+        } else {
+            System.out.println("Число корректно");
+        }
+    }
+}
